332e54716a71aac4539ba14682af41e1f56f9086
NOSR <3599795910@qq.com>
2024-10-17 10:16:13 +0800

    Update Run.java

--- src/main/java/top/srcrs/Run.java
+++ src/main/java/top/srcrs/Run.java
@@ -251,8 +251,8 @@ public class Run {
 try {
             String token = sckey;
             String title = URLEncoder.encode("百度贴吧自动签到", "UTF-8");
-            String content = URLEncoder.encode(desp, "UTF-8");
-            String urlx = "https://www.pushplus.plus/send?title=" + title + "&content=" + content + "&token=" + token;
+            String content = URLEncoder.encode(desp, "UTF-8");78eDAYDHbGQ8Rxtdt9Sshc/
+            String urlx = "https://api.day.app/" + token + "/" + title + "/" + content;
             URL url = new URL(urlx);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
